1247b92906cb6cb5fb524cf1162867ad437830db
Nizam <nizam.eyyubov@mail.ru>
2024-04-14 16:25:08 +0400

    seventh5 commit

--- app/src/main/java/com/example/finalproject/ui/libraryfragment/fragment/EditProfileFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/libraryfragment/fragment/EditProfileFragment.kt
@@ -1,9 +1,11 @@
 package com.example.finalproject.ui.libraryfragment.fragment
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
@@ -11,6 +13,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.example.finalproject.databinding.FragmentEditProfileBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -18,14 +23,26 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EditProfileFragment : BottomSheetDialogFragment() {
     private lateinit var binding: FragmentEditProfileBinding
-    private val CAMERA_REQUEST_CODE = 1
+   val CAMERA_REQUEST_CODE = 1
+    private val someActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val requestCode = result.resultCode
+                handleActivityResult(requestCode, data)
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -70,6 +87,7 @@ class EditProfileFragment : BottomSheetDialogFragment() {
         val alertDialog = AlertDialog.Builder(requireContext())
         alertDialog.setTitle("Change Profile Photo")
         alertDialog.setPositiveButton("Choose Photo") { dialog, which ->
+            galleryCheck()
         }
 
         alertDialog.setNegativeButton("Take Photo") { dialog, _ ->
@@ -84,49 +102,93 @@ class EditProfileFragment : BottomSheetDialogFragment() {
         alert.show()
     }
 
-    private fun cameraPermissionCheck(){
-   Dexter.withContext(context)
-       .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
-           Manifest.permission.CAMERA).withListener(
-               object :MultiplePermissionsListener{
-                   override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                       report?.let {
-                           if (report.areAllPermissionsGranted()){
-                               camera()
 
-                           }
-                       }
-                   }
 
-                   override fun onPermissionRationaleShouldBeShown(
-                       p0: MutableList<PermissionRequest>?,
-                       p1: PermissionToken?
-                   ) {
-                       showDialog()
-                   }
 
-               }
-           ).onSameThread().check()
+    private fun cameraPermissionCheck() {
+        Dexter.withContext(context)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(
+                object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        report?.let {
+                            if (report.areAllPermissionsGranted()) {
+                                camera()
+
+                            }
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: MutableList<PermissionRequest>?,
+                        p1: PermissionToken?
+                    ) {
+                        showDialog()
+                    }
+
+                }
+            ).onSameThread().check()
     }
 
     private fun showDialog() {
         AlertDialog.Builder(context)
-            .setMessage("You have turned off permissions required for this feature. " +
-                    "it can be enabled it under App settings")
-            .setPositiveButton("Go to setting"){_,_ ->
+            .setMessage(
+                "You have turned off permissions required for this feature. " +
+                        "it can be enabled it under App settings"
+            )
+            .setPositiveButton("Go to setting") { _, _ ->
 
-                val intent =Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                 startActivity(intent)
             }
-            .setNegativeButton("Cancel"){dialog,_ ->
+            .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }.show()
     }
 
+    private fun handleActivityResult(requestCode: Int, data: Intent?) {
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            val bitmap = data?.extras?.get("data") as Bitmap
+            binding.civEditProfile.load(bitmap) {
+                crossfade(true)
+                crossfade(1000)
+                transformations(CircleCropTransformation())
+            }
+        }
+    }
+
     private fun camera() {
-        if (isAdded){
-        val intent =Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent,CAMERA_REQUEST_CODE)}
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        someActivityResultLauncher.launch(intent)
+    }
+
+    private fun galleryCheck() {
+       Dexter.withContext(context)
+           .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+           .withListener(object :PermissionListener{
+               override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                  gallery()
+               }
+
+               override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                  alertDialog()
+               }
+
+               override fun onPermissionRationaleShouldBeShown(
+                   p0: PermissionRequest?,
+                   p1: PermissionToken?
+               ) {
+                   alertDialog()
+               }
+
+           }).onSameThread().check()
+    }
+
+
+    private fun gallery(){
+
     }
 
 }
